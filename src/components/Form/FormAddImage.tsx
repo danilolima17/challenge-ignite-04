58d36194f4ba518c0b2631bee9959e312e20e8ec
danilolima17 <danilodavite20@hotmail.com>
2023-02-27 19:38:56 -0300

    first commit

---
+++ src/components/Form/FormAddImage.tsx
@@ -0,0 +1,152 @@
+import { Box, Button, Stack, useToast } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+
+import { api } from '../../services/api';
+import { FileInput } from '../Input/FileInput';
+import { TextInput } from '../Input/TextInput';
+
+interface FormAddImageProps {
+  closeModal: () => void;
+}
+
+export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
+  const [imageUrl, setImageUrl] = useState('');
+  const [localImageUrl, setLocalImageUrl] = useState('');
+  const toast = useToast();
+
+  function validateImageSize(image: File): string | undefined {
+    if (image.size > 10_000_000) {
+      return 'O arquivo deve ser menor que 10MB';
+    }
+    return undefined;
+  }
+
+  function validateAcceptedFormats(image: File): string | undefined {
+    const regex = new RegExp(/image\/(jpeg|png|gif)/);
+    const isFormatValid = regex.test(image.type);
+    return isFormatValid
+      ? undefined
+      : 'Somente são aceitos arquivos PNG, JPEG e GIF';
+  }
+
+  const formValidations = {
+    image: {
+      required: {
+        message: 'Arquivo obrigatório',
+        value: true,
+      },
+      validate: {
+        lessThan10MB: (value: File[]) => validateImageSize(value[0]),
+        acceptedFormats: (value: File[]) => validateAcceptedFormats(value[0]),
+      },
+    },
+    title: {
+      required: 'Título obrigatório',
+      minLength: {
+        value: 2,
+        message: 'Mínimo de 2 caracteres',
+      },
+      maxLength: {
+        value: 20,
+        message: 'Máximo de 20 caracteres',
+      },
+    },
+    description: {
+      required: 'Descrição obrigatória',
+      maxLength: {
+        value: 65,
+        message: 'Máximo de 65 caracteres',
+      },
+    },
+  };
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    (data: { title: string; description: string; url: string }) => {
+      return api.post('api/images', data);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['images']);
+      },
+    }
+  );
+
+  const { register, handleSubmit, reset, formState, setError, trigger } =
+    useForm();
+  const { errors } = formState;
+
+  const onSubmit = async (data: Record<string, unknown>): Promise<void> => {
+    try {
+      if (!imageUrl) {
+        toast({
+          title: 'Imagem não adicionada',
+          description:
+            'É preciso adicionar e aguardar o upload de uma imagem antes de realizar o cadastro',
+        });
+        return;
+      }
+      await mutation.mutateAsync({
+        title: data?.title as string,
+        description: data?.description as string,
+        url: imageUrl,
+      });
+
+      toast({
+        title: 'Imagem cadastrada',
+        description: 'Sua imagem foi cadastrada com sucesso.',
+      });
+    } catch {
+      toast({
+        title: 'Falha no cadastro',
+        description: 'Ocorreu um erro ao tentar cadastrar a sua imagem',
+      });
+    } finally {
+      setImageUrl('');
+      setLocalImageUrl('');
+      reset();
+      closeModal();
+    }
+  };
+
+  return (
+    <Box as="form" width="100%" onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={4}>
+        <FileInput
+          setImageUrl={setImageUrl}
+          localImageUrl={localImageUrl}
+          setLocalImageUrl={setLocalImageUrl}
+          setError={setError}
+          trigger={trigger}
+          error={errors.image}
+          {...register('image', formValidations.image)}
+        />
+
+        <TextInput
+          placeholder="Título da imagem..."
+          error={errors.title}
+          {...register('title', formValidations.title)}
+        />
+
+        <TextInput
+          placeholder="Descrição da imagem..."
+          error={errors.description}
+          {...register('description', formValidations.description)}
+        />
+      </Stack>
+
+      <Button
+        my={6}
+        isLoading={formState.isSubmitting}
+        isDisabled={formState.isSubmitting}
+        type="submit"
+        w="100%"
+        py={6}
+      >
+        Enviar
+      </Button>
+    </Box>
+  );
+}
---
+++ src/components/Modal/ViewImage.tsx
@@ -0,0 +1,37 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalFooter,
+  ModalBody,
+  Image,
+  Link,
+} from '@chakra-ui/react';
+
+interface ModalViewImageProps {
+  isOpen: boolean;
+  onClose: () => void;
+  imgUrl: string;
+}
+
+export function ModalViewImage({
+  isOpen,
+  onClose,
+  imgUrl,
+}: ModalViewImageProps): JSX.Element {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent bgColor="pGray.800" w="fit-content" maxW="full">
+        <ModalBody w="fit-content" maxW="full" p={0}>
+          <Image src={imgUrl} alt="" maxW={900} maxH={600} objectFit="cover" />
+        </ModalBody>
+        <ModalFooter>
+          <Link href={imgUrl} target="_blank" mr="auto">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
